34a133b29ab704792f8c1a9eea70aace86f3998a
qfg978 <qfg978@alumni.ku.dk>
2024-03-21 13:13:00 +0100

    panik

--- inauguralproject/inauguralproject.py
+++ inauguralproject/inauguralproject.py
@@ -1,3 +1,59 @@
+#Imports
+import matplotlib.pyplot as plt
+plt.rcParams.update({"axes.grid":True,"grid.color":"black","grid.alpha":"0.25","grid.linestyle":"--"})
+plt.rcParams.update({'font.size': 14})
+%load_ext autoreload
+%autoreload 2
+
+#Questions
+
+#1.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 def square(x):
     """ square numpy array
     
@@ -13,3 +69,4 @@ def square(x):
     
     y = x**2
     return y
+
