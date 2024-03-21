bc0766af0479fad1f67e683a1ddd264bace0190a
qfg978 <qfg978@alumni.ku.dk>
2024-03-21 13:22:08 +0100

    huh

--- inauguralproject/inauguralproject.py
+++ inauguralproject/inauguralproject.py
@@ -1,13 +1,42 @@
-#Imports
-import matplotlib.pyplot as plt
-plt.rcParams.update({"axes.grid":True,"grid.color":"black","grid.alpha":"0.25","grid.linestyle":"--"})
-plt.rcParams.update({'font.size': 14})
-%load_ext autoreload
-%autoreload 2
+from types import SimpleNamespace
 
-#Questions
+class ExchangeEconomyClass:
 
-#1.
+    def __init__(self):
+
+        par = self.par = SimpleNamespace()
+
+        # a. preferences
+        par.alpha = 1/3
+        par.beta = 2/3
+
+        # b. endowments
+        par.w1A = 0.8
+        par.w2A = 0.3
+
+    def utility_A(self,x1A,x2A):
+        pass
+
+    def utility_B(self,x1B,x2B):
+        pass
+
+    def demand_A(self,p1):
+        pass
+
+    def demand_B(self,p1):
+        pass
+
+    def check_market_clearing(self,p1):
+
+        par = self.par
+
+        x1A,x2A = self.demand_A(p1)
+        x1B,x2B = self.demand_B(p1)
+
+        eps1 = x1A-par.w1A + x1B-(1-par.w1A)
+        eps2 = x2A-par.w2A + x2B-(1-par.w2A)
+
+        return eps1,eps2
 
 
 
