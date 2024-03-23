00977c25056e9a32e06a55f3d8fad9542723bdf4
rdt689 <rdt689@alumni.ku.dk>
2024-03-23 17:36:10 +0100

    wuhu

--- inauguralproject/inauguralproject.py
+++ inauguralproject/inauguralproject.py
@@ -15,16 +15,20 @@ class ExchangeEconomyClass:
         par.w2A = 0.3
 
     def utility_A(self,x1A,x2A):
-        pass
+        par = self.par
+        return (x1A ** par.alpha) * (x2A ** par.beta)
 
     def utility_B(self,x1B,x2B):
-        pass
+        par = self.par
+        return (x1B ** par.beta) * (x2B ** par.alpha)
 
     def demand_A(self,p1):
-        pass
+        par = self.par
+        return par.alpha * ( par.w1A + par.w2A / p1), par.beta * (p1 * par.w1A + par.w2A)
 
     def demand_B(self,p1):
-        pass
+        par = self.par
+        return par.beta * (( 1 - par.w1A) +  (1 - par.w2A ) / p1), par.alpha * (p1 * (1 - par.w1A) + (1 - par.w2A))
 
     def check_market_clearing(self,p1):
 
@@ -37,12 +41,6 @@ class ExchangeEconomyClass:
         eps2 = x2A-par.w2A + x2B-(1-par.w2A)
 
         return eps1,eps2
-
-
-
-
-
-
 
 
 
